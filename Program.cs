6f85d56fb827c1c7e50024bba3a318d649c4a886
Douglas <douglas.ferreirag@gmail.com>
2022-09-20 22:14:11 -0300

    break

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ int numero = 5;
 int contador = 0;
 while(contador <= 10){
 
-        Console.WriteLine($"{contador}ª  * Execução: {numero} x contador = {numero * contador}");
+        Console.WriteLine($"{contador + 1}ª  * Execução: {numero} x contador = {numero * contador}");
 
+        
+        
         contador++;
+
+        if (contador == 6){
+
+                    break;
+
+        }
 
 }
 
